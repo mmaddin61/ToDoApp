c9b82b0e5b73909a223ceca78143029ac96bc5c5
Michael Maddin <michaelmaddinii@gmail.com>
2024-11-18 16:58:29 -0500

    Add test data factories

---
+++ app/src/main/java/com/group1/todoapp/data/Datasource.kt
@@ -0,0 +1,37 @@
+package com.group1.todoapp.data
+
+import com.group1.todoapp.TaskData
+import com.group1.todoapp.TodoData
+
+class Datasource {
+    private fun taskDataFactory(count: Int = 1): MutableList<TaskData> {
+        val tasks: MutableList<TaskData> = emptyList<TaskData>().toMutableList()
+        for (i in 1..count) {
+            tasks.add(
+                TaskData(
+                    title = "Task $i",
+                    description = "Description for Task $i.",
+                    finished = i%2==0 // Set true if i is even
+                )
+            )
+        }
+        return tasks
+    }
+
+    private fun testDataFactory(listCount: Int = 1, taskCount: Int = 1): List<TodoData> {
+        val todoLists: MutableList<TodoData> = emptyList<TodoData>().toMutableList()
+        for (i in 1..listCount) {
+            todoLists.add(
+                TodoData(
+                    title = "Test List $i",
+                    tasks = taskDataFactory(taskCount)
+                )
+            )
+        }
+        return todoLists.toList()
+    }
+
+    fun fetchToDoLists(): List<TodoData> {
+        return testDataFactory(listCount = 3, taskCount = 5)
+    }
+}
